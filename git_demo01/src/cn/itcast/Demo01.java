d1aaf7b17f482db868186792a4164198db10eb76
Kino <kino@cw.com>
2019-05-19 20:02:40 +0800

    提交git_demo01

---
+++ git_demo01/src/cn/itcast/Demo01.java
@@ -0,0 +1,11 @@
+package cn.itcast;
+
+/**
+ * @Author Yuki
+ * @Date 2019/5/19 20:00
+ */
+public class Demo01 {
+    public static void main(String[] args) {
+        System.out.println("11111111111111111111");
+    }
+}
