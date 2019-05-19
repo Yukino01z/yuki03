e22711564ebeb879b959c745edebaeac717c8171
Kino <kino@cw.com>
2019-05-19 20:29:50 +0800

    修改git_demo01

--- git_demo01/src/cn/itcast/Demo01.java
+++ git_demo01/src/cn/itcast/Demo01.java
@@ -7,5 +7,6 @@ package cn.itcast;
 public class Demo01 {
     public static void main(String[] args) {
         System.out.println("11111111111111111111");
+        System.out.println("22222222222222222222");
     }
 }
